3d30d21a2ed8d838c00ddc9234061e736f812ded
Chaos <antonioscutari2@hotmail.com>
2022-10-19 21:46:02 -0300

    Sólo para chusmear

---
+++ tokenMock.js
@@ -0,0 +1,17 @@
+tokenazo = {
+  iss: "https://accounts.google.com",
+  nbf: 1666154554,
+  aud: "281986241956-9lc4tsccm664e97kpgd5h7vnp2dr087k.apps.googleusercontent.com",
+  sub: "113285504591347403800",
+  email: "pinkylobotomy@gmail.com",
+  email_verified: true,
+  azp: "281986241956-9lc4tsccm664e97kpgd5h7vnp2dr087k.apps.googleusercontent.com",
+  name: "Juan Carlos Sukkadili",
+  picture:
+    "https://lh3.googleusercontent.com/a/ALm5wu34IZdkofe_CU7f3642_U5oyMgwVwiVvEVOxky2RcM=s96-c",
+  given_name: "Juan",
+  family_name: "Sukkadili",
+  iat: 1666154854,
+  exp: 1666158454,
+  jti: "26a30409fad178d8f16fd0fc1c9bb8cae8d9ee49",
+};
